b5618ecded87cc0eaf0dcbf9c86ab239b2f4d6d0
Boriswaikin <plmnboris@gmail.com>
2024-04-22 04:53:01 -0700

    update directory

--- client/EV_testing.py
+++ client/EV_testing.py
@@ -7,7 +7,7 @@ from client.globalDefinition import (API_KEY)
 from client.SOC import (
     getStateOfCharge
 )
-from Elevation import elevation_difference
+from client.Elevation import elevation_difference
 from geopy.distance import geodesic
 import matplotlib.pyplot as plt
 from geopy.distance import geodesic
